477d3c1bd70f8015c6bd94fb8c5d21b66983ee85
Gerhard Weis <gerhard.weis@gmail.com>
2013-10-02 14:27:54 +1000

    moved various Mock classes to testing module

---
+++ src/gu/z3cform/rdf/testing.py
@@ -0,0 +1,113 @@
+import os.path
+from ordf.graph import Graph, ConjunctiveGraph
+from gu.z3cform.rdf.interfaces import IORDF
+from zope.interface import implementer
+from gu.z3cform.rdf.fresnel.fresnel import Fresnel
+from rdflib import URIRef
+from plone.z3cform.fieldsets.extensible import ExtensibleForm
+from z3c.form import form
+from z3c.form.interfaces import DISPLAY_MODE
+from zope.component import getUtility
+from rdflib import RDF
+
+
+class MockHandler(object):
+
+    store = None
+
+    _cache = None
+
+    def __init__(self):
+        self._cache = {}
+        self.store = ConjunctiveGraph()
+
+    def put(self, graph):
+        # copy given graph into store
+        # remove old data
+        self.store.remove_context(graph.identifier)
+        # add new data
+        for s, p, o in graph:
+            self.store.add((s, p, o, graph.identifier))
+        # remove from _cache
+        if graph.identifier in self._cache:
+            del self._cache[graph.identifier]
+
+    def get(self, identifier):
+        # simple check out mechanism.
+        # the handler returns the same graph as long as it's not put back
+        if identifier in self._cache:
+            return self._cache[identifier]
+        graph = self.store.get_context(identifier)
+        # make a copy of the graph
+        cgraph = Graph(identifier=identifier)
+        for t in graph:
+            cgraph.add(t)
+        self._cache[identifier] = cgraph
+        return cgraph
+
+    def query(self, query):
+        return self.store.query(query)
+
+
+@implementer(IORDF)
+class MockIORDFUtility(object):
+
+    fresnel = None
+    handler = None
+
+    def getHandler(self):
+        if self.handler is None:
+            self.handler = MockHandler()
+        return self.handler
+
+    def getFresnel(self):
+        # TODO: make fresnel_defs configurable for different test scenarios
+        if self.fresnel is None:
+            formatgraph = Fresnel()
+            formatgraph.parse(os.path.join(os.path.dirname(__file__),
+                                           'tests/test_defs.ttl'),
+                              format='turtle')
+            formatgraph.compile()
+            self.fresnel = formatgraph
+        return self.fresnel
+
+    def getBaseURI(self):
+        return u"http://example.com/"
+
+    def generateURI(self):
+        import uuid
+        uri = u"{}{}".format(self.getBaseURI(), uuid.uuid1())
+        return URIRef(uri)
+
+
+class ExtensibleAddForm(ExtensibleForm, form.AddForm):
+
+    def create(self, data):
+        # create object
+        content = Graph()
+        # apply form.rdftype
+        content.add((content.identifier, RDF['type'], self.rdftype))
+        # apply form data
+        form.applyChanges(self, content, data)
+        for group in self.groups:
+            form.applyChanges(group, content, data)
+        return content
+
+    def add(self, object):
+        # put stuff into store via handler
+        # FIXME: has already been added by create. see above
+        getUtility(IORDF).getHandler().put(object)
+
+    def nextURL(self):
+        # nothing useful to do here
+        self.request.getURL()
+
+
+class ExtensibleEditForm(ExtensibleForm, form.EditForm):
+
+    pass
+
+
+class ExtensibleDisplayForm(ExtensibleForm, form.Form):
+
+    mode = DISPLAY_MODE
--- src/gu/z3cform/rdf/tests/test_doc.py
+++ src/gu/z3cform/rdf/tests/test_doc.py
@@ -1,97 +1,10 @@
 import doctest
 import unittest
-import os.path
 from z3c.form import testing
 from z3c.form import outputchecker
-from z3c.form import form
-from z3c.form.interfaces import DISPLAY_MODE
 from zope.configuration import xmlconfig
-from zope.interface import implementer
-from gu.z3cform.rdf.interfaces import IORDF
-from gu.z3cform.rdf.fresnel.fresnel import Fresnel
-from zope.component import provideUtility, getUtility
-from plone.z3cform.fieldsets.extensible import ExtensibleForm
-from ordf.graph import Graph, ConjunctiveGraph
-from rdflib import RDF, URIRef
-
-
-class MockHandler(object):
-
-    store = None
-
-    def put(self, graph):
-        for s, p, o in graph:
-            self.store.add((s, p, o, graph.identifier))
-
-    def get(self, identifier):
-        graph = self.store.get_context(identifier)
-        return Graph(store=graph.store,
-                     identifier=graph.identifier)
-
-
-@implementer(IORDF)
-class MockIORDFUtility(object):
-
-    fresnel = None
-    handler = None
-
-    def getHandler(self):
-        if self.handler is None:
-            self.handler = MockHandler()
-        return self.handler
-
-    def getFresnel(self):
-        if self.fresnel is None:
-            formatgraph = Fresnel()
-            formatgraph.parse(os.path.join(os.path.dirname(__file__),
-                                           'test_defs.ttl'),
-                              format='turtle')
-            formatgraph.compile()
-            self.fresnel = formatgraph
-        return self.fresnel
-
-    def getBaseURI(self):
-        return u"http://example.com/"
-
-    def generateURI(self):
-        import uuid
-        uri = u"{}{}".format(self.getBaseURI(), uuid.uuid1())
-        return URIRef(uri)
-
-
-class ExtensibleAddForm(ExtensibleForm, form.AddForm):
-
-    def create(self, data):
-        # create object
-        content =  Graph()
-        # apply form.rdftype
-        content.add((content.identifier, RDF['type'], self.rdftype))
-        # apply form data
-        # FIXME: we have a problem here. the datamanager already puts it into the
-        #        hnadler.
-        form.applyChanges(self, content, data)
-        for group in self.groups:
-            form.applyChanges(group, content, data)
-        return content
-
-    def add(self, object):
-        # put stuff into store via handler
-        # FIXME: has already been added by create. see above
-        getUtility(IORDF).getHandler().put(object)
-
-    def nextURL(self):
-        # nothing useful to do here
-        self.request.getURL()
-
-
-class ExtensibleEditForm(ExtensibleForm, form.EditForm):
-
-    pass
-
-
-class ExtensibleDisplayForm(ExtensibleForm, form.Form):
-
-    mode = DISPLAY_MODE
+from zope.component import provideUtility
+from gu.z3cform.rdf.testing import MockIORDFUtility
 
 
 def setUp(test):
